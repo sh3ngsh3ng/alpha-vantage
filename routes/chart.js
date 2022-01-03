db5b28bc41af9ed82d85ee33b061db5f57905334
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-03 18:02:34 +0000

    candlestick chart data faulty

--- routes/chart.js
+++ routes/chart.js
@@ -14,7 +14,6 @@ router.get("/:ticker", async(req,res) => {
     }
     let response = await axios.get(url, axiosConfig)
     let rawDailyCandleData = response.data["Time Series (Daily)"]
-    console.log(rawDailyCandleData)
     /* 
         RAW DATA => 
         '2021-08-11': {
@@ -31,7 +30,33 @@ router.get("/:ticker", async(req,res) => {
             y: [O,H,L,C]
         }]
     */
-    res.render("chart")
+   let candleStickData = []
+   for (let candleKey in rawDailyCandleData) {
+       // candleKey is the date in YYYY-MM-DD
+        let candleObj = {}
+        let candle = rawDailyCandleData[candleKey]
+        // Date
+        let year = parseInt(candleKey.slice(0, 4))
+        let month = parseInt(candleKey.slice(6, 7))
+        let day = parseInt(candleKey.slice(8))
+        let date = new Date(year,month,day)
+        let timeStamp = date.getTime()
+        candleObj.x = timeStamp
+        // OHLC
+        let open = parseFloat(candle["1. open"])
+        let high = parseFloat(candle["2. high"])
+        let low = parseFloat(candle["3. low"])
+        let close = parseFloat(candle["4. close"])
+        let volume = parseFloat(candle["5. volume"])
+        let y = [open, high, low, close]
+        candleObj.y = y
+        candleStickData.push(candleObj)
+   }
+   console.log(candleStickData)
+    res.render("chart", {
+        candleStickData
+    })
+    // res.send(candleStickData)
 
 })
 
